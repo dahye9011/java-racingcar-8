0301f13826ff25e3200da23ecb160d09eaa66268
dahye <dahye9011@naver.com>
2025-10-23 16:38:10 +0900

    refactor(CarTest): 중복 메서드 이름 수정 및 매개변수 순서 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,29 +1,12 @@
 package racingcar;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
-    @Test
-    @DisplayName("자동차 이름을 쉼표(,)를 기준으로 구분한다.")
-    void 자동차_이름_구분() {
-        // given
-        CarNameParser parser = new CarNameParser();
-        String input = "a,b,c";
-
-        // when
-        List<String> result = parser.nameParse(input);
-
-        // then
-        assertThat(result).contains("a", "b", "c");
-    }
-
     @ParameterizedTest
     @DisplayName("이동 값이 4 미만인 경우, 전진하지 않는다.")
     @ValueSource(ints = {0, 1, 2, 3})
@@ -35,13 +18,13 @@ public class CarTest {
         car.move(input);
 
         // then
-        assertEquals(car.getPosition(), 0);
+        assertEquals(0, car.getPosition());
     }
 
     @ParameterizedTest
     @DisplayName("이동 값이 4 이상인 경우, 전진한다.")
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    void 전진_테스트(int input) {
+    void 전진_테스트2(int input) {
         // given
         Car car = new Car("rye");
 
@@ -49,6 +32,6 @@ public class CarTest {
         car.move(input);
 
         // then
-        assertEquals(car.getPosition(), 1);
+        assertEquals(1, car.getPosition());
     }
 }
