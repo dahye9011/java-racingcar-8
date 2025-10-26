815bdde4bc1f758b74b6d43e61c12330f5792269
dahye <dahye9011@naver.com>
2025-10-26 11:05:02 +0900

    feat(Car): 자동차 이름 검증 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,16 +1,17 @@
 package racingcar;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
     private static final int MINIMUM_TO_MOVE = 4;
     private final String name;
     private int position;
 
     public Car(String name) {
         this.name = name;
+        validateName();
         this.position = 0;
     }
 
-    // input == 무작위 값
     public void move(int input) {
         if (input >= MINIMUM_TO_MOVE) {
             this.position++;
@@ -32,5 +33,15 @@ public class Car {
             return "";
         }
         return "-".repeat(this.position);
+    }
+
+    private void validateName() {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+        }
+
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 }
