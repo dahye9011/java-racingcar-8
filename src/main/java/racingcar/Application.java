79a1cad816947fd345e94ff4fe951063df5c0e02
dahye <dahye9011@naver.com>
2025-10-27 14:01:16 +0900

    feat(Application): 애플리케이션 main 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,12 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Parser parser = new Parser();
+
+        RacingCarController controller = new RacingCarController(inputView, outputView, parser);
+
+        controller.run();
     }
 }
